b1a9cd94ba65171913b1d793e493862c6b5335e4
huuhuybnPC <huuhuybn@gmail.com>
2022-02-12 17:19:56 +0700

    hjgjg

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ function App() {
     const [link, setLink] = useState();
     return (
         <div className="App">
-            <header className="App-header">
-                <h1 className="App-link">TikTok Video Downloader</h1>
-                <input style={{borderWidth : 1,borderColor : 'blue',padding : 10, borderRadius : 5, width : '60%'}}
-                       placeholder={'Enter Tiktok Link'}/>
-                <button style={{ height : 50,width : 100, marginTop : 8}} title={'Download'} onClick={() => {
 
+            <header className="App-header">
+                <h1 className="App-link">PINTEREST VIDEO DOWNLOADER</h1>
+                <h2>Download Video Pinterest HD, 2K</h2>
+
+                <div style={{flexDirection : 'row', width : '100%'}}>
+                <input style={{borderWidth: 1, borderColor: 'blue',height : 40, borderRadius: 5}}
+                       placeholder={'Enter Pinterest Link'}/>
+                <button style={{ height : 40}} title={'Download'} onClick={() => {
                     const myHeaders = new Headers({
                         "x-rapidapi-host": "tiktok-video-no-watermark2.p.rapidapi.com",
                         "x-rapidapi-key": "2ede18d1bfmsh717c6e42745b666p11ca16jsn68dd7903ab84",
@@ -26,20 +29,30 @@ function App() {
 
                 }} download>Download
                 </button>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+              </div>
+
+
             </header>
             <div>
+                DOWNLOAD VIDEO PINTEREST
+            </div>
+            <div>
+                DotSave is best download Video Pinterest and Photo Pinterest website/tool.
+                <br/>
+                It works for computers, tablets and mobile devices.
+                <br/>
+                Download Pinterest Video Full HD, 2K, 4K
+            </div>
+            <div>
+                FEATURE
+            </div>
 
+            <div>
+                Download Pinterest Video with FullHD, 720p, 320p ...
+                <br/>
+                Download Pinterest Photo with Original Size, Large, Medium, Thumbnail size ...
+                <br/>
+                Download Pinterest Video without installing anythings
             </div>
 
 
