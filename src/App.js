6ffbb85ff4861eb8ac2fb9e35483319e081249f2
huuhuybnPC <huuhuybn@gmail.com>
2022-02-13 23:11:58 +0700

    hjgjg

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
         <div className="App">
 
             <header className="App-header">
-                <h1 className="App-link">PINTEREST VIDEO DOWNLOADER</h1>
-                <h2 style={{color : 'black'}}>Download Video Pinterest HD, 2K</h2>
+                <h1 style={{color: 'black'}}><span style={{color: 'red'}}>PINTEREST</span> VIDEO DOWNLOADER</h1>
+                <h2 style={{color: 'black'}}>Full HD, 2K. All Devices</h2>
+                <div className="ui input">
+                    <input type="text" placeholder="Enter Pinterest Link..."/>
+                </div>
 
-                <div style={{flexDirection : 'row', width : '100%'}}>
-                <input style={{borderWidth: 1, borderColor: 'blue',height : 40,width : '100%', borderRadius: 5}}
-                       placeholder={'Enter Pinterest Link'}/>
-                <button style={{ height : 40, marginTop : 16}} title={'Download'} onClick={() => {
+                <button className="ui labeled icon button" style={{marginTop : 8}} title={'Download'} onClick={() => {
                     const myHeaders = new Headers({
                         "x-rapidapi-host": "tiktok-video-no-watermark2.p.rapidapi.com",
                         "x-rapidapi-key": "2ede18d1bfmsh717c6e42745b666p11ca16jsn68dd7903ab84",
@@ -26,33 +26,41 @@ function App() {
                     }).then(r => r.json()).then(data => {
                         setLink(data.data.play)
                     });
-
-                }} download>Download
+                }} download><i className="download icon"></i>Download
                 </button>
-              </div>
+
+
+
 
 
             </header>
             <div>
-                DOWNLOAD VIDEO PINTEREST
-            </div>
-            <div>
-                DotSave is best download Video Pinterest and Photo Pinterest website/tool.
-                <br/>
-                It works for computers, tablets and mobile devices.
-                <br/>
-                Download Pinterest Video Full HD, 2K, 4K
-            </div>
-            <div>
-                FEATURE
-            </div>
 
-            <div>
-                Download Pinterest Video with FullHD, 720p, 320p ...
-                <br/>
-                Download Pinterest Photo with Original Size, Large, Medium, Thumbnail size ...
-                <br/>
-                Download Pinterest Video without installing anythings
+                <div style={{width: '100%', color: 'black'}}>
+
+                    <div>
+                        DOWNLOAD VIDEO PINTEREST
+                    </div>
+                    <div>
+                        DotSave is best download Video Pinterest and Photo Pinterest website/tool.
+                        <br/>
+                        It works for computers, tablets and mobile devices.
+                        <br/>
+                        Download Pinterest Video Full HD, 2K, 4K
+                    </div>
+                </div>
+
+                <div>
+                    FEATURE
+                </div>
+
+                <div>
+                    Download Pinterest Video with FullHD, 720p, 320p ...
+                    <br/>
+                    Download Pinterest Photo with Original Size, Large, Medium, Thumbnail size ...
+                    <br/>
+                    Download Pinterest Video without installing anythings
+                </div>
             </div>
 
 
